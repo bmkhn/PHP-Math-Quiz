c7cb8e7aa1dfc31b23174e6aa9d85d984fb3003c
Julius Vincent D. Llavan <202280209@psu.palawan.edu.ph>
2024-12-01 00:22:13 +0800

    Set Up Basic HTML Layout

---
+++ client.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Mathematics</title>
+</head>
+<body>
+    <div>
+        <h1>Simple Mathematics</h1>
+
+        <!-- START QUIZ FORM -->
+        <!-- SETTINGS BUTTON -->
+        <!-- RESULTS SECTION -->
+        <!-- QUIZ SECTION -->
+        <!-- SETTINGS SECTION -->
+    </div> <!-- Container Div -->
+</body>
+</html>
