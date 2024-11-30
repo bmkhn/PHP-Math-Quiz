bddea391f4d447e2c27d7c6b911df82d85ae40c8
Julius Vincent D. Llavan <202280209@psu.palawan.edu.ph>
2024-12-01 02:56:57 +0800

    Finalize client.php

--- client.php
+++ client.php
@@ -18,12 +18,14 @@ $quizActive = isset($_SESSION['quiz']);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple Mathematics</title>
+
     <style>
         .hidden { display: none; }
-        .container { border: 1px solid black; padding: 15px; width: 405px; }
+        .container { border: 1px solid black; padding: 0px 25px 25px 25px; width: 405px; }
         fieldset { display: inline-block; }
         body { display: flex; align-items: center; justify-content: center;}
     </style>
+
 </head>
 <body>
     <div class="container">
